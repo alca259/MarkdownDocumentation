df5c9e3dfdffab3951232efe7c8e3c986f77070c
Alca259 <alca259@gmail.com>
2023-01-30 16:08:22 +0100

    Added help info

--- src/MarkdownDocumentation/MDConsole/ConsoleOption.cs
+++ src/MarkdownDocumentation/MDConsole/ConsoleOption.cs
@@ -5,15 +5,16 @@ public sealed class ConsoleOption
     private const string COMMAND_PREFIX = "-";
     private const string COMMAND_FULL_PREFIX = "--";
 
-    public ConsoleOption(string command, string commandFull)
+    public ConsoleOption(string command, string commandFull, string helpInfo = null)
 	{
         Command = command;
         CommandFull = commandFull;
+        HelpInfo = helpInfo;
     }
-
 
     public string Command { get; }
     public string CommandFull { get; }
+    public string HelpInfo { get; }
     public string Value { get; private set; }
 
     public List<string> GetOptions()
--- src/MarkdownDocumentation/MDConsole/Program.cs
+++ src/MarkdownDocumentation/MDConsole/Program.cs
@@ -7,13 +7,13 @@ internal static class Program
 {
     private static readonly List<ConsoleOption> _consoleOptions = new()
     {
-        new ConsoleOption("i", "input"),
-        new ConsoleOption("o", "output")
+        new ConsoleOption("i", "input", $"Puedes especificar la ruta relativa o absoluta de ficheros XML de entrada.{Environment.NewLine}\t\tEstos deben ser los generados de documentación por el compilador.{Environment.NewLine}\t\tPor defecto .\\Input"),
+        new ConsoleOption("o", "output", $"Puedes especificar la ruta relativa o absoluta de la salida de ficheros Markdown.{Environment.NewLine}\t\tPor defecto: .\\Output")
     };
 
     private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ShowHelp();
         SetArgumentValues(args);
 
         var inputFolder = _consoleOptions.Single(s => s.CommandFull == "input").Value ?? @".\\Input";
@@ -36,7 +36,7 @@ internal static class Program
         var mdGenerator = new MarkdownGenerator(types, outputFolder);
         await mdGenerator.DoIt();
 
-        Console.WriteLine("Bye, World!");
+        Console.WriteLine($"Documentación generada en {new DirectoryInfo(outputFolder).FullName}");
     }
 
     private static void SetArgumentValues(string[] args)
@@ -53,5 +53,16 @@ internal static class Program
     {
         var value = args.SkipWhile(i => !consoleOption.GetOptions().Contains(i.ToLower().Trim())).Skip(1).Take(1).FirstOrDefault();
         consoleOption.SetValue(value);
+    }
+
+    private static void ShowHelp()
+    {
+        Console.WriteLine("### Ayuda ###");
+        Console.WriteLine("Puedes utilizar las siguientes opciones para configurar esta aplicación:");
+        foreach (var option in _consoleOptions)
+        {
+            Console.WriteLine($"\t- Opción => {string.Join(", ", option.GetOptions())}");
+            if (!string.IsNullOrWhiteSpace(option.HelpInfo)) Console.WriteLine($"\t\t{option.HelpInfo}");
+        }
     }
 }
